a1a60eed2e2f79643bc503320aec36065e6e6c7a
apoptosis1831 <apoptosis1831@gmail.com>
2022-05-21 16:35:49 +0900

    FEAT user login

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ app.post('/api/users/login', (req,res)=>{
     User.findOne({ email : req.body.email}, (err,user)=>{
         if(!user){
             return res.json({
-                loginSucess : false,
+                loginSuccess : false,
                 message : '제공된 이메일에 해당하는 유저가 없다'
             })
         }
@@ -57,7 +57,7 @@ app.post('/api/users/login', (req,res)=>{
         // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인
         user.comparePassword(req.body.password, (err,isMatch)=>{
             if(!isMatch)
-            return res.json({loginSucess: false, message : "비밀번호가 틀렸습니다"})
+            return res.json({loginSuccess: false, message : "비밀번호가 틀렸습니다"})
             
             // 비밀번호 맞다면 토근을 생성하기
             user.generateToken((err,user)=>{
@@ -66,7 +66,7 @@ app.post('/api/users/login', (req,res)=>{
                 //토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
                 res.cookie("x_auth", user.token)
                 .status(200)
-                .json({loginSucess: true, userId : user._id})
+                .json({loginSuccess: true, userId : user._id})
 
 
 
